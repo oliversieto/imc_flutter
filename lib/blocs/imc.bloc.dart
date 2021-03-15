ca13a48cb7e4a4c7d0c0322bf5f60daf9edd1227
Renato Oliveira <oliversieto@gmail.com>
2021-03-14 21:42:03 -0400

    refactor: criado o cálculo do imc

---
+++ lib/blocs/imc.bloc.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+class ImcBloc {
+  MaskedTextController heightCtrl = MaskedTextController(mask: '000');
+  MaskedTextController weightCtrl = MaskedTextController(mask: '000');
+  String result = "Preencha os dados para calcular seu IMC";
+
+  calculate() {
+    double height = double.parse(heightCtrl.text);
+    double weight = double.parse(weightCtrl.text);
+    double imc = weight / (height * height);
+
+    if (imc >= 40) {
+      result = "Obesidade Grau III (${imc.toStringAsPrecision(2)})";
+    } else if (imc >= 35) {
+      result = "Obesidade Grau II (${imc.toStringAsPrecision(2)})";
+    } else if (imc >= 30) {
+      result = "Obesidade Grau I (${imc.toStringAsPrecision(2)})";
+    } else if (imc >= 25) {
+      result = "Levemente acima do peso (${imc.toStringAsPrecision(2)})";
+    } else if (imc >= 18.5) {
+      result = "Peso ideal (${imc.toStringAsPrecision(2)})";
+    } else {
+      result = "Abaixo do peso (${imc.toStringAsPrecision(2)})";
+    }
+  }
+}
