f12c363ef41afa15bcf2d73d9f64af3a268f8b02
Renato Oliveira <oliversieto@gmail.com>
2021-03-14 21:10:40 -0400

    refactor: criado a home para o android

--- lib/ui/pages/home.page.dart
+++ lib/ui/pages/home.page.dart
@@ -8,6 +8,54 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cálculo de IMC"),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: "Altura (cm)",
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: "Peso (kg)",
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Você está fora de forma",
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: TextButton(
+                child: Text(
+                  "Calcular",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
