63b8f0df7d9bfd20a544b593f1f6c1073ca533eb
Renato Oliveira <oliversieto@gmail.com>
2021-03-14 21:27:08 -0400

    refactor: criado a home page para o iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:imc/ui/pages/home.page.dart';
+import 'package:imc/ui/android/material-app.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Imc',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-    );
-  }
+  runApp(MyMaterialApp());
 }
